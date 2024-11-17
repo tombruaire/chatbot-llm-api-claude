d958e1d9320b7da799a464750175189daf905aa0
tombr <tombruaire@gmail.com>
2024-11-17 09:26:48 +0100

    Installation de nodemon

--- serveur/app.js
+++ serveur/app.js
@@ -1,10 +1,28 @@
+const { Anthropic } = require('@anthropic-ai/sdk');
 const express = require('express')
 const app = express()
 const port = 3001
+const anthropic = new Anthropic({
+  apiKey: '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', // defaults to process.env["ANTHROPIC_API_KEY"]
+});
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Appel de l\'api de Claude')
 })
+
+async function   
+ sendMessage() {
+  const msg = await anthropic.messages.create({
+    model: "claude-3-5-sonnet-20241022",
+    max_tokens: 1024,
+    messages: [{ role: "user", content: "Hello, Claude" }],
+  });
+  console.log(msg);   
+
+}
+
+sendMessage(); 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
